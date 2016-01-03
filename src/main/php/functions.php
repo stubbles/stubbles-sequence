ff7f0ac459c30359bd9a276a2a8f0ac70447fe1a
mikey179 <mikey@stubbles.net>
2016-01-03 13:19:12 +0100

    add castToArray() from core

--- src/main/php/functions.php
+++ src/main/php/functions.php
@@ -10,6 +10,67 @@
 namespace stubbles\sequence {
 
     /**
+     * cast given value into an array
+     *
+     * The following casts are applied:
+     * - array: returned as is
+     * - instance of \Traversable: return value from iterator_to_array()
+     * - object with asArray() method: returns value from call to this method
+     * - object with toArray() method: returns value from call to this method
+     * - object: returns map of properties using extractObjectProperties()
+     * - any other: returns array with value as single entry
+     *
+     * @param   mixed  $value
+     * @return  array
+     * @since   5.4.0
+     */
+    function castToArray($value)
+    {
+        if ($value instanceof \Traversable) {
+            return iterator_to_array($value);
+        } elseif (is_array($value)) {
+            return $value;
+        } elseif (is_object($value)) {
+            if (method_exists($value, 'asArray')) {
+                return $value->asArray();
+            } elseif (method_exists($value, 'toArray')) {
+                return $value->toArray();
+            }
+
+            return extractObjectProperties($value);
+        }
+
+        return [$value];
+    }
+
+    /**
+     * method to extract all properties regardless of their visibility
+     *
+     * This is a workaround for the problem that as of PHP 5.2.4 get_object_vars()
+     * is not any more capable of retrieving private properties from child classes.
+     * See http://stubbles.org/archives/32-Subtle-BC-break-in-PHP-5.2.4.html.
+     *
+     * @param   object  $object
+     * @return  array
+     * @since   3.1.0
+     */
+    function extractObjectProperties($object)
+    {
+        $properties      = (array) $object;
+        $fixedProperties = [];
+        foreach ($properties as $propertyName => $propertyValue) {
+            if (!strstr($propertyName, "\0")) {
+                $fixedProperties[$propertyName] = $propertyValue;
+                continue;
+            }
+
+            $fixedProperties[substr($propertyName, strrpos($propertyName, "\0") + 1)] = $propertyValue;
+        }
+
+        return $fixedProperties;
+    }
+
+    /**
      * ensures that the given callable is really callable
      *
      * Internal functions like strlen() or is_string() require an *exact* amount
